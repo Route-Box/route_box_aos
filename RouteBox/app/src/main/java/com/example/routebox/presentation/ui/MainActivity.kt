501f5fb09b0c93913dddc60cd447ecf08b393fbc
Nahyun Kim <hedghog1004@naver.com>
2024-07-06 01:02:49 +0900

    ✨ Feat: navigation으로 바텀네비게이션 화면 이동 구현
    - navigation 의존성 추가
    - BottomNavigation 아이템 클릭 시 화면 이동
    - StatusBar 색상 변경
    
    Related to: #1

--- RouteBox/app/src/main/java/com/example/routebox/presentation/ui/MainActivity.kt
+++ RouteBox/app/src/main/java/com/example/routebox/presentation/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.routebox.presentation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.routebox.R
 import com.example.routebox.databinding.ActivityMainBinding
 
@@ -13,10 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        setBottomNavigation()
+
+        initNavigation()
     }
 
-    private fun setBottomNavigation() {
+    private fun initNavigation() {
         binding.mainBottomNav.itemIconTintList = null
+
+        NavigationUI.setupWithNavController(binding.mainBottomNav, findNavController(R.id.main_nav_host))
     }
 }
