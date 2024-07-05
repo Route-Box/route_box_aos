39d2b1bfca8a9653326404e15b38c25dd7e4c3ec
Nahyun Kim <hedghog1004@naver.com>
2024-07-06 00:28:24 +0900

    ✨ Feat: 바텀네비게이션 UI 구현
    - 아이콘 추가 및 selector 생성
    - menu 설정
    
    Related to: #1

--- RouteBox/app/src/main/java/com/example/routebox/presentation/ui/MainActivity.kt
+++ RouteBox/app/src/main/java/com/example/routebox/presentation/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.routebox.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.routebox.R
+import com.example.routebox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        binding.mainBottomNav.itemIconTintList = null
     }
 }
