e5c52f1224776b7418efc692123bb658c573fdac
Nahyun Kim <hedghog1004@naver.com>
2024-07-16 02:00:35 +0900

    🐛 Fix: 뒤로가기 관련 오류 수정
    - 뒤로가기 2번 클릭 시 앱 종료
    
    Resolves: #7
    Related to: #1

--- RouteBox/app/src/main/java/com/example/routebox/presentation/ui/MainActivity.kt
+++ RouteBox/app/src/main/java/com/example/routebox/presentation/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.routebox.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -12,16 +13,40 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var backPressedTime: Long = 0 // 뒤로가기 버튼을 눌렀던 시간 저장
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+
         initNavigation()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        setAppFinishedFlow()
     }
 
     private fun initNavigation() {
         binding.mainBottomNav.itemIconTintList = null
 
         NavigationUI.setupWithNavController(binding.mainBottomNav, findNavController(R.id.main_nav_host))
+    }
+
+    private fun setAppFinishedFlow() {
+        if (System.currentTimeMillis() > backPressedTime + BACK_PRESSED_DURATION) {
+            backPressedTime = System.currentTimeMillis()
+            Toast.makeText(this, "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // 2초 이내에 뒤로가기 버튼을 한 번 더 누르면 앱 종료
+        if (System.currentTimeMillis() <= backPressedTime + BACK_PRESSED_DURATION) {
+            finish()
+        }
+    }
+
+    companion object {
+        const val BACK_PRESSED_DURATION = 2_000L
     }
 }
