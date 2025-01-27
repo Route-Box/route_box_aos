86316768f52eddf9fff4464414bc84a737c76dda
JM2308 <wlals2987@naver.com>
2025-01-28 00:13:40 +0900

    🐛 Fix: 탐색 홈 오류 수정
    - 탐색 홈 새로고침 오류 수정
    - 내 지갑 포인트 내역 더미데이터 삭제
    
    Related to: #55

--- RouteBox/app/src/main/java/com/daval/routebox/presentation/ui/seek/SeekViewModel.kt
+++ RouteBox/app/src/main/java/com/daval/routebox/presentation/ui/seek/SeekViewModel.kt
@@ -1,5 +1,6 @@
 package com.daval.routebox.presentation.ui.seek
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -43,7 +44,6 @@ class SeekViewModel @Inject constructor(
     fun getRouteList() {
         viewModelScope.launch {
             val getRouteListResult = repository.getSearchRouteList(_page.value!!, ROUTE_SIZE).result
-
             if (getRouteListResult.size != 0) {
                 _routeList.value = getRouteListResult
             }
