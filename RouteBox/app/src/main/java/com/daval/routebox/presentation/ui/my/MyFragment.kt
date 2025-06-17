de34f08a55cc49fc48488af92b25ae2b5ed26d9a
JM2308 <wlals2987@naver.com>
2025-06-17 22:29:26 +0900

    ♻ Refactor: 마이페이지 웹뷰 코드 제거
    Related to: #90

--- RouteBox/app/src/main/java/com/daval/routebox/presentation/ui/my/MyFragment.kt
+++ RouteBox/app/src/main/java/com/daval/routebox/presentation/ui/my/MyFragment.kt
@@ -1,36 +1,15 @@
 package com.daval.routebox.presentation.ui.my
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebViewClient
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import com.google.gson.Gson
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
 import com.daval.routebox.databinding.FragmentMyBinding
-import com.daval.routebox.domain.model.MessageType
-import com.daval.routebox.domain.model.NativeTokenRequestMessage
-import com.daval.routebox.domain.model.TokenPayload
-import com.daval.routebox.domain.model.WebViewPage
-import com.daval.routebox.presentation.config.ApplicationClass.Companion.dsManager
-import com.daval.routebox.presentation.config.Constants.ENDPOINT_MY
-import com.daval.routebox.presentation.config.Constants.WEB_BASE_URL
 import com.daval.routebox.presentation.ui.auth.LoginActivity
-import com.daval.routebox.presentation.utils.NativeMessageCallback
-import com.daval.routebox.presentation.utils.WebViewBridge
-import kotlinx.coroutines.launch
 
-class MyFragment : Fragment(), NativeMessageCallback {
+class MyFragment : Fragment() {
     private lateinit var binding: FragmentMyBinding
 
     override fun onCreateView(
@@ -41,101 +20,12 @@ class MyFragment : Fragment(), NativeMessageCallback {
     ): View {
         binding = FragmentMyBinding.inflate(inflater, container, false)
 
-        initWebViewSetting()
-
         return binding.root
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun initWebViewSetting() {
-        // 웹뷰 설정
-        binding.myWebView.apply {
-            // 세팅
-            settings.javaScriptEnabled = true // JavaScript를 사용한 웹뷰를 로드한다면 활성화 필요
-            settings.loadWithOverviewMode = true // 컨텐츠의 크기가 WebView 보다 클 경우, 스크린에 맞게 자동 조정
-            settings.domStorageEnabled = true // 웹뷰에서 LocalStorage를 사용해야 하는 경우 활성화 필요
-
-            addJavascriptInterface(object : WebViewBridge(this@MyFragment) {}, WebViewBridge.INTF)
-            webViewClient = WebViewClient()
-            loadUrl("$WEB_BASE_URL$ENDPOINT_MY") // 웹 주소
-
-            // WebView 뒤로가기 설정
-            setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
-                if (event.action != KeyEvent.ACTION_DOWN) return@OnKeyListener true
-                // 뒤로가기 버튼을 눌렀을 때, WebView에서 뒤로가기가 된다면 뒤로가고 아니라면 종료
-                if (keyCode == KeyEvent.KEYCODE_BACK) {
-                    if (this.canGoBack()) {
-                        this.goBack()
-                    } else {
-                        requireActivity().onBackPressedDispatcher.addCallback(object :
-                            OnBackPressedCallback(true) {
-                            override fun handleOnBackPressed() {}
-                        })
-                    }
-                    return@OnKeyListener true
-                }
-                false
-            })
-        }
-    }
-
-    // 네이티브 앱에서 웹뷰로 TOKEN 메시지 보내기
-    private fun sendTokenToWebView() {
-        // 0.5초 지연 후 토큰 전송
-        Handler(Looper.getMainLooper()).postDelayed({
-            binding.myWebView.evaluateJavascript("javascript:sendMessageToWebView(${getRequestMessage()})", null)
-        }, 500)
-    }
-
-    private fun getRequestMessage(): String {
-        val nativeMessage = NativeTokenRequestMessage(
-            type = MessageType.TOKEN.text,
-            payload = TokenPayload(getSavedAccessToken())
-        )
-
-        return Gson().toJson(nativeMessage)
-    }
-
-    // 앱 내 저장된 토큰 정보 가져오기
-    private fun getSavedAccessToken(): String = runBlocking {
-        dsManager.getAccessToken().first().orEmpty()
-    }
-
-    override fun onReactComponentLoaded(boolean: Boolean) {
-        sendTokenToWebView()
-    }
-
-    override fun onMessageReceived(type: MessageType, page: WebViewPage, id: String) {
-        //
-    }
-
-    override fun onMyPageMessageReceive(page: WebViewPage) {
-        when (page) {
-            WebViewPage.LOGOUT -> { // 로그아웃
-                Log.d("MyFragment", "Logout")
-                lifecycleScope.launch {
-                    deleteToken()
-                    moveToLoginActivity()
-                }
-            }
-            WebViewPage.WITHDRAW -> { // 회원탈퇴
-                lifecycleScope.launch {
-                    Log.d("MyFragment", "Withdraw")
-                    deleteToken()
-                    moveToLoginActivity()
-                }
-            }
-            else -> Log.d("MyFragment", "Unknown page: $page")
-        }
-    }
-
+    // MEMO: 웹뷰에서 로그인 액티비티로 이동을 위해 썼던 코드 / UI 제작 후 사용 안할 시 제거
     private fun moveToLoginActivity() {
         requireActivity().startActivity(Intent(requireActivity(), LoginActivity::class.java))
         requireActivity().finish()
-    }
-
-    // 앱 내에 저장된 토큰 정보 삭제
-    private suspend fun deleteToken() {
-        dsManager.clearTokens()
     }
 }
